4631f35fb47b71468b07e0e6d1dcf0c5a0f177c1
Lalit Sahu <lalit.sahu@iphtechnologies.com>
2026-07-30 18:29:54 +0530

    remove navbar

--- E-Commerce-app/src/components/Home/Home.jsx
+++ E-Commerce-app/src/components/Home/Home.jsx
@@ -7,10 +7,12 @@ import Category from "../category/Category";
 import SellingFlash from "../sellingProducts/SellingFlash";
 import MusicBanner from "../MusicBanner/MusicBanner"
 import ExploreProducts from "../ExploreProducts/ExploreProducts";
+import Featured from "../Featured/Featured";
+import Footer from "../Footer/Footer";
 export default function Home(){
     return(
         <>
-        <Navbar/>
+        {/* <Navbar/> */}
         <div className="home">
             <Sidebar/>
             <Hero/>
@@ -23,6 +25,8 @@ export default function Home(){
 
         <MusicBanner/>
         <ExploreProducts/>
+        <Featured/>
+        <Footer/>
         </>
     )
 }
