f1661373f45d70654272c3c7def1c6cb246ed450
Lalit Sahu <lalit.sahu@iphtechnologies.com>
2026-08-05 16:10:51 +0530

    feat: update home page sections and layout

--- E-Commerce-app/src/components/Home/Home.jsx
+++ E-Commerce-app/src/components/Home/Home.jsx
@@ -1,32 +1,27 @@
 import Sidebar from "../Sidebar/Sidebar";
-import Navbar from "../Navbar/Navbar";
-import "./home.css"
 import Hero from "../Hero/Hero";
 import FlashSale from "../product/FlashSale";
 import Category from "../category/Category";
 import SellingFlash from "../sellingProducts/SellingFlash";
-import MusicBanner from "../MusicBanner/MusicBanner"
+import MusicBanner from "../MusicBanner/MusicBanner";
 import ExploreProducts from "../ExploreProducts/ExploreProducts";
 import Featured from "../Featured/Featured";
-import Footer from "../Footer/Footer";
-export default function Home(){
-    return(
-        <>
-        {/* <Navbar/> */}
-        <div className="home">
-            <Sidebar/>
-            <Hero/>
-            
-        </div>
-        <FlashSale/>
+import "./home.css";
 
-        <Category/>
-        <SellingFlash/>
+export default function Home() {
+  return (
+    <>
+      <div className="home">
+        <Sidebar />
+        <Hero />
+      </div>
 
-        <MusicBanner/>
-        <ExploreProducts/>
-        <Featured/>
-        <Footer/>
-        </>
-    )
+      <FlashSale />
+      <Category />
+      <SellingFlash />
+      <MusicBanner />
+      <ExploreProducts />
+      <Featured />
+    </>
+  );
 }
