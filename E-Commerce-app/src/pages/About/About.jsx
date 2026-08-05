b4889deb005774511006d966616cc37bd0c38177
Lalit Sahu <lalit.sahu@iphtechnologies.com>
2026-08-05 16:12:32 +0530

    feat: update about page content and layout

--- E-Commerce-app/src/pages/About/About.jsx
+++ E-Commerce-app/src/pages/About/About.jsx
@@ -1,6 +1,5 @@
 import "./about.css";
 import { Link } from "react-router-dom";
-import Footer from "../../components/Footer/Footer";
 
 import aboutImg from "../../assets/about/about.png";
 
@@ -97,7 +96,6 @@ export default function About() {
         </div>
       </section>
 
-      <Footer />
     </>
   );
 }
