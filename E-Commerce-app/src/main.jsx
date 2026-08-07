2b86aca662a0070c176f44b80749c6bb05b3c5df
Lalit Sahu <lalit.sahu@iphtechnologies.com>
2026-08-07 16:46:32 +0530

    Register Toast provider

--- E-Commerce-app/src/main.jsx
+++ E-Commerce-app/src/main.jsx
@@ -8,6 +8,7 @@ import { CartProvider } from "./context/CartContext";
 import { SearchProvider } from "./context/SearchContext";
 import { CategoryProvider } from "./context/CategoryContext";
 import ScrollToTop from "./components/ScrollToTop";
+import { ToastProvider } from "./context/ToastContext";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
@@ -16,7 +17,10 @@ ReactDOM.createRoot(document.getElementById("root")).render(
      < CategoryProvider >
      <CartProvider>
       <WishlistProvider>
-            <App />
+        <ToastProvider>
+        <App />
+
+        </ToastProvider>
        
       </WishlistProvider>
       </CartProvider>
