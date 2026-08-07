8e68ec40be7069e97f24940181d88e78584108c0
Lalit Sahu <lalit.sahu@iphtechnologies.com>
2026-08-07 16:44:38 +0530

    Update selling products functionality

--- E-Commerce-app/src/components/sellingProducts/SellingProducts.jsx
+++ E-Commerce-app/src/components/sellingProducts/SellingProducts.jsx
@@ -12,34 +12,80 @@ import { useWishlist } from "../../context/WishlistContext";
 import { useCart } from "../../context/CartContext";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useToast } from "../../context/ToastContext";
 
 export default function SellingProducts({ product }) {
   console.log(product);
-  const { addToCart } = useCart();
+  const {
+    addToCart,
+    removeFromCart,
+    isInCart,
+  } = useCart();
   const { toggleWishlist, isInWishlist } = useWishlist();
-  const [added, setAdded] = useState(false);
+  // const [added, setAdded] = useState(false);
   const navigate = useNavigate();
-
-  const handleAddCart = () => {
-    addToCart(product);
-
-    setAdded(true);
-
-    setTimeout(() => {
-      setAdded(false);
-    }, 1000);
-  };
+  const{showToast} = useToast()
 
   const handleWishlist = () =>{
-    const currentUser = JSON.parse(localStorage.getItem("currentUser"));
+    const currentUser = JSON.parse(
+      localStorage.getItem("currentUser")
+    );
 
     if(!currentUser){
-      alert("please login first to add products to wishlist.");
+      // alert("please login first to add products to wishlist.");
+      showToast("please login first to add products to wishlist.", "error")
       navigate("/signup");
       return;
     }
     toggleWishlist(product);
+    if (alreadyInWishlist) {
+      showToast("Product removed from wishlist.", "success");
+    } else {
+      showToast("Product added to wishlist.", "success");
+    }
   }
+  // handle add to catt
+  // const handleAddCart = () => {
+  //   const currentUser = JSON.parse(localStorage.getItem("currentUser"));
+  
+  //   if (!currentUser) {
+  //     // alert("Please login first to add products to cart.");
+  //     showToast("please login first to add products to Cart.", "error")
+
+  //     navigate("/signup");
+  //     return;
+  //   }
+  
+  //   addToCart(product);
+  //   showToast("Product added to cart.", "success");
+  //   setAdded(true);
+  
+  //   setTimeout(() => {
+  //     setAdded(false);
+  //   }, 1000);
+  // };
+  const handleAddCart = () => {
+    const currentUser = JSON.parse(
+      localStorage.getItem("currentUser")
+    );
+  
+    if (!currentUser) {
+      showToast(
+        "Please login first to add products to cart.",
+        "error"
+      );
+      navigate("/signup");
+      return;
+    }
+  
+    if (isInCart(product.id)) {
+      removeFromCart(product.id);
+      showToast("Product removed from cart.", "success");
+    } else {
+      addToCart(product);
+      showToast("Product added to cart.", "success");
+    }
+  };
   return (
     <div className="product-card">
       <div className="product-image">
@@ -76,10 +122,14 @@ export default function SellingProducts({ product }) {
 
         <img src={product.image} alt={product.title} />
         <button
-          className={`cart-btn ${added ? "added" : ""}`}
+         className={`cart-btn ${
+          isInCart(product.id) ? "added" : "removed"
+        }`}
           onClick={handleAddCart}
         >
-          {added ? "✓ Added" : "Add To Cart"}
+         {isInCart(product.id)
+  ? "Remove Item"
+  : "Add to Cart"}
         </button>
       </div>
 
