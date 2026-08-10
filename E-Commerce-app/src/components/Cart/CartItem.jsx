53e9b8ad368be62c74a8ccd70e37fd34b66c72e5
Lalit Sahu <lalit.sahu@iphtechnologies.com>
2026-08-10 16:55:54 +0530

    update using redux

--- E-Commerce-app/src/components/Cart/CartItem.jsx
+++ E-Commerce-app/src/components/Cart/CartItem.jsx
@@ -1,75 +1,105 @@
 import {
-    
-    FaChevronUp,
-    FaChevronDown,
-  } from "react-icons/fa";
-  import {IoClose} from "react-icons/io5"
-  
-  import { useCart } from "../../context/CartContext";
-  
-  export default function CartItem({ product }) {
-  
-    const {
-      increaseQty,
-      decreaseQty,
-      removeFromCart,
-    } = useCart();
-  
-    return (
-      <div className="cart-row">
-  
-        <div className="cart-product">
-  
-          <img
-            src={product.image}
-            alt={product.title}
-          />
-  
-          <span>{product.title}</span>
-  
-          <button
-            className="remove-item"
-            onClick={() => removeFromCart(product.id)}
-          >
-            <IoClose />
-          </button>
-  
-        </div>
-  
-        <div>
-          ${product.price}
-        </div>
-  
-        <div>
-  
-          <div className="qty-box">
-  
-            <span>{product.quantity}</span>
-  
-            <div className="qty-icons">
-  
-              <FaChevronUp
-                onClick={() =>
-                  increaseQty(product.id)
-                }
-              />
-  
-              <FaChevronDown
-                onClick={() =>
-                  decreaseQty(product.id)
-                }
-              />
-  
-            </div>
-  
-          </div>
-  
-        </div>
-  
-        <div>
-          ${product.price * product.quantity}
-        </div>
-  
+  FaChevronUp,
+  FaChevronDown,
+} from "react-icons/fa";
+
+import { IoClose } from "react-icons/io5";
+
+import { useDispatch } from "react-redux";
+import { removeFromCart, decreaseQuantity,increaseQuantity, clearCart } from "../../redux/slices/cartSlice";
+
+// import { useCart } from "../../context/CartContext";
+
+export default function CartItem({ product }) {
+
+  // ================================
+  // OLD CART CONTEXT
+  // ================================
+
+  // const {
+  //   increaseQty,
+  //   decreaseQty,
+  //   removeFromCart,
+  // } = useCart();
+
+
+  // ================================
+  // REDUX
+  // ================================
+
+  const dispatch = useDispatch();
+
+
+  return (
+    <div className="cart-row">
+
+      <div className="cart-product">
+
+        <img
+          src={product.image}
+          alt={product.title}
+        />
+
+        <span>{product.title}</span>
+
+        <button
+          className="remove-item"
+          onClick={() =>
+            dispatch(removeFromCart(product.id))
+          }
+        >
+          <IoClose />
+        </button>
+
       </div>
-    );
-  }
+
+
+      <div>
+        ${product.price}
+      </div>
+
+
+      <div>
+
+        <div className="qty-box">
+
+          <span>
+            {product.quantity || 1}
+          </span>
+
+          <div className="qty-icons">
+
+            <FaChevronUp
+              onClick={() =>
+                dispatch(
+                  increaseQuantity(product.id)
+                )
+              }
+            />
+
+            <FaChevronDown
+              onClick={() =>
+                dispatch(
+                  decreaseQuantity(product.id)
+                )
+              }
+            />
+
+          </div>
+
+        </div>
+
+      </div>
+
+
+      <div>
+        $
+        {(
+          product.price *
+          (product.quantity || 1)
+        ).toFixed(2)}
+      </div>
+
+    </div>
+  );
+}
